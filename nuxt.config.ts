e3c4377abcdbe1a536855a9dc0de1058b746396d
kom1lov001 <avazkomilov94@gmail.com>
2026-01-17 00:14:20 +0500

    minor changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
   runtimeConfig: {
     public: {
-      emailService: '',
-      emailTemplate: '',
-      emailKey: ''
-    }
+      emailService: process.env.NUXT_PUBLIC_EMAIL_SERVICE,
+      emailTemplate: process.env.NUXT_PUBLIC_EMAIL_TEMPLATE,
+      emailKey: process.env.NUXT_PUBLIC_EMAIL_KEY,
+    },
   },
 
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
 
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1.0',
-      title: 'Vezra Studio',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      title: "Vezra Studio",
       link: [
         {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
         },
         {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com',
-          crossorigin: ''
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
         },
         {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&family=Nunito:wght@400;600;700&display=swap'
-        }
-      ]
-    }
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&family=Nunito:wght@400;600;700&display=swap",
+        },
+      ],
+    },
   },
 
-  css: ['~/assets/css/main.scss'],
+  css: ["~/assets/css/main.scss"],
 
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          api: 'modern-compiler'
-        }
-      }
-    }
-  }
-})
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
+});
